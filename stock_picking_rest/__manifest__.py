347ef2e425c536afdd93d1ad61c739ac92404ce0
AudisoftWilson <wcontreras@audisoft.com>
2024-05-31 19:30:10 -0500

    [AT-02][FIX] Ajuste al manifest
    El manifest no contemplaba dependencias con modelo de compras y contactos, por lo que la instalación en un ambiente sin estos módulos, genera un error

--- stock_picking_rest/__manifest__.py
+++ stock_picking_rest/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','product', 'sale'],
+    'depends': ['base','stock','contacts', 'sale', 'purchase'],
 
     # always loaded
     'data': [
